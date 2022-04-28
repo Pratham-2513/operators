aed7ef22d096fb52d894e6faeea0d8fabc5d7c4e
Pratham-2513 <104135367+Pratham-2513@users.noreply.github.com>
2022-04-28 15:02:27 +0530

    Update Arithematical.c

--- Arithematical.c
+++ Arithematical.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int x,y,sum,difference,product,division;
+    int x,y,sum,difference,product,modulus;
+    float division;
     scanf("%d %d",&x,&y);
     sum=x+y;
     difference=x-y;
     product=x*y;
     division=x/y;
-    printf("The sum of x and y is %d\nThe difference of x and y is %d\nThe product of x and y is %d\nThe quotitent of x and y is %d", sum,difference,product,division);
+    modulus=x%y;
+    printf("The sum of x and y is %d\nThe difference of x and y is %d\nThe product of x and y is %d\nThe quotitent of x and y is %.2f\nThe remainder of x and y is %d", sum,difference,product,division);
     return 0;
 }
